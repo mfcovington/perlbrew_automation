4a3698c71009b193d5baf488c6cf372975d34f48
Mike Covington <mfcovington@gmail.com>
2012-05-09 23:35:26 -0700

    autocpanm_perlbrew.sh currently functional when used as: ./autocpanm_perlbrew.sh Mod::Ule

---
+++ autocpanm_perlbrew.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+export PERLBREW_ROOT=` which perlbrew | perl -e '<> =~ m|(.*)/bin/perlbrew|; print $1' `
+export PERLBREW_HOME=/tmp/.perlbrew
+source ${PERLBREW_ROOT}/etc/bashrc
+
+module=$1
+current=` perlbrew switch | perl -e '<> =~ m/(perl.*)/; print $1' `
+
+for ver in `perlbrew available | grep -e "^i" | cut -c 3-`
+do
+    perlbrew use $ver
+    perlbrew switch $ver
+    perlbrew switch
+    echo "Installing $module for $ver"
+    cpanm $module
+    echo "Testing installation of $module"
+    perl -e "use $module; print $]"
+    echo; echo
+done
+
+echo "Re-testing installation of $module"
+perlbrew exec perl -e "use $module; print $]"
+
+echo "Switching back to $current"
+perlbrew switch $current
